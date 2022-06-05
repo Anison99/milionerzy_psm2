dc4dc84768e9bae1205b1fec0dce62dd5844a86e
Szczepan Wójcik <szczep1000@outlook.com>
2022-06-05 17:59:17 +0200

    Remove unused methods in App singleton

--- app/src/main/java/com/example/mil_app/App.java
+++ app/src/main/java/com/example/mil_app/App.java
@@ -1,18 +1,14 @@
 package com.example.mil_app;
 
 import android.app.Application;
-import android.content.res.Resources;
 
 public class App extends Application {
 	private static App mInstance;
-	private static Resources res;
-	
 	
 	@Override
 	public void onCreate() {
 		super.onCreate();
 		mInstance = this;
-		res = getResources();
 	}
 	
 	public static App getInstance() {
