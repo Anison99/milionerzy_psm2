24e7aac70cef1ba07f7eeba0e0182a4242afc4b3
Szczepan Wójcik <szczep1000@outlook.com>
2022-06-05 13:33:53 +0200

    CSV controller fetching questions from App singleton. Also updated java from 11 to recent one.

--- app/src/main/java/com/example/mil_app/database/CSVController.java
+++ app/src/main/java/com/example/mil_app/database/CSVController.java
@@ -1,12 +1,15 @@
 package com.example.mil_app.database;
 
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.mil_app.App;
+import com.example.mil_app.R;
+
 import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
-import java.net.URL;
+import java.io.InputStreamReader;
 import java.util.Random;
 
-public class CSVController {
+public class CSVController extends AppCompatActivity {
 	/**
 	 * Amount of rows associated with question in each individual .csv file.
 	 */
@@ -18,13 +21,25 @@ public class CSVController {
 	 * @return {@link Question} object
 	 */
 	public static Question getRandomQuestion(int csvNumber) {
+		int resolvedResource = switch (csvNumber) {
+			case 1 -> R.raw.q1;
+			case 2 -> R.raw.q2;
+			case 3 -> R.raw.q3;
+			case 4 -> R.raw.q4;
+			case 5 -> R.raw.q5;
+			case 6 -> R.raw.q6;
+			case 7 -> R.raw.q7;
+			case 8 -> R.raw.q8;
+			case 9 -> R.raw.q9;
+			case 10 ->R.raw.q10;
+			case 11 -> R.raw.q11;
+			case 12 -> R.raw.q12;
+			default -> throw new IllegalStateException("Unexpected value: " + csvNumber);
+		};
 		try {
-			System.out.println("CSV number: " + csvNumber);
-			URL csvFileLocation = CSVController.class.getResource(csvNumber + ".csv");
-			System.out.println("CSV resource location url: " + csvFileLocation);
-			File CSVFile = new File(csvFileLocation.getPath());
-			BufferedReader br = new BufferedReader(new FileReader(CSVFile));
+			BufferedReader br = new BufferedReader(new InputStreamReader(App.getInstance().getResources().openRawResource(resolvedResource)));
 			int rand = new Random().nextInt(LINES_IN_CSV); // random from 0 to lines-1
+			System.out.println("Random:"+rand);
 			for (int i = 0; i <= rand - 1; i++) {
 				br.readLine();
 			}
@@ -36,8 +51,7 @@ public class CSVController {
 			byte correct = Byte.parseByte(record[5]);
 			String[] answersArray = new String[4];
 			System.arraycopy(record, 1, answersArray, 0, 4);
-			int questionNumber = csvNumber;
-			return new Question(questionNumber, question, answersArray, correct,
+			return new Question(csvNumber, question, answersArray, correct,
 					getPrize(csvNumber));
 		} catch (Exception e) {
 			System.out.println(e.getMessage());
@@ -46,32 +60,20 @@ public class CSVController {
 	}
 	
 	private static int getPrize(int stageNumber) {
-		switch (stageNumber) {
-			case 1:
-				return 500;
-			case 2:
-				return 1000;
-			case 3:
-				return 2000;
-			case 4:
-				return 5000;
-			case 5:
-				return 10000;
-			case 6:
-				return 20000;
-			case 7:
-				return 40000;
-			case 8:
-				return 75000;
-			case 9:
-				return 125000;
-			case 10:
-				return 250000;
-			case 11:
-				return 500000;
-			case 12:
-				return 1000000;
-		}
-		return stageNumber;
+		return switch (stageNumber) {
+			case 1 -> 500;
+			case 2 -> 1000;
+			case 3 -> 2000;
+			case 4 -> 5000;
+			case 5 -> 10000;
+			case 6 -> 20000;
+			case 7 -> 40000;
+			case 8 -> 75000;
+			case 9 -> 125000;
+			case 10 -> 250000;
+			case 11 -> 500000;
+			case 12 -> 1000000;
+			default -> stageNumber;
+		};
 	}
 }
